a00b18641509d3b5e8ceea6b42c005092ccdb71b
bangzhen fan <719577252@qq.com>
2019-11-03 22:46:17 +0800

    Initial commit

---
+++ src/components/index.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+  <table>
+    <tr>
+      <th>用户id</th>
+      <th>用户名</th>
+      <th>用户密码</th>
+
+      <th>删除状态</th>
+      <th>修改时间</th>
+      <th>操作</th>
+    </tr>
+    <tr v-for="(item ,index) in users">
+      <td>{{item.id}}</td>
+      <td>{{item.name}}</td>
+      <td>{{item.password}}</td>
+      <td>
+        {{item.isDel}}
+      </td>
+      <td>{{item.updateTime}}</td>
+      <td>
+
+       <button @click="handleDeleteUser(item.id)">删除</button>
+      </td>
+
+    </tr>
+  </table>
+</div>
+
+</template>
+<script>
+export default {
+  data(){
+    return{
+    }
+  },
+  methods:{
+    handleDeleteUser(id){
+      console.log(id);
+      this.$store.dispatch('ajaxDeleteUser',id)
+    }
+  },
+  created:function () {
+    this.$store.dispatch('ajaxGetUser')
+  },
+
+  computed:{
+    users() {
+      // return this.$store.getters.getQuestionList;
+      let _data=this.$store.getters.getUserList;
+      return _data;
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,66 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from "vuex"
+import axios from "axios"
+Vue.config.productionTip = false
+
+Vue.use(Vuex)
+
+const store=new Vuex.Store({
+  state:{
+    userList:[],
+  },
+  getters:{
+    getUserList(state){
+      return state.userList;
+    },
+  },
+  mutations:{
+    setUserList(state,data){
+      this.state.userList=data;
+    },
+  },
+  actions:{
+    async ajaxGetUser(context){
+      let result=await axios.get('app/api/users').then(response=>{
+        console.log("axios请求userList得到"+JSON.stringify(response.data))
+        context.commit('setUserList',response.data.userList)
+        return response.data.userList;
+      });
+      return result
+    },
+    async ajaxDeleteUser(context,id) {
+      console.log("请求delete的userid为", id);
+      axios.post('/app/api/user/update',
+        JSON.stringify({
+          id: id,
+          isDel: 1
+        }),{
+          headers:{
+            'Content-Type': 'application/json'
+          }
+        }
+        )
+        .then(function (response) {
+          context.dispatch('ajaxGetUser')
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+})
+
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,store,
+  components: { App },
+  template: '<App/>'
+})
